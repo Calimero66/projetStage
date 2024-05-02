ecddef9990866a92598614fb1104136b306ff1b9
hamza aboulmaali 'Calim <rica.hamza@yahoo.fr>
2024-05-02 09:28:24 +0100

    Update

--- frontend/src/pages/Admin/CreateManager.jsx
+++ frontend/src/pages/Admin/CreateManager.jsx
@@ -1,66 +1,64 @@
 import React from 'react'
 
-import { BiUserCircle } from "react-icons/bi";
-import { AiOutlineLock } from "react-icons/ai";
-
 const CreateManager = () => {
     return (
-        <div className='h-[calc(100vh-180px)] flex justify-center items-center'>
-            
-            <div className='border rounded-md p-16 shadow-lg relative'>
-                <h1 className=' text-3xl text-[#3C3633] py-2.5 ' >Create Manager</h1>
-                <form action="">
+        <>
 
-                    <div className=' relative my-4' >
+            <div className='h-[calc(100vh-180px)] flex justify-center items-center'>
+                <div className='border rounded-md p-12 shadow-lg relative'>
+                    {/* <h1 className=' text-3xl text-[#3C3633] text-center ' >Create Manager</h1> */}
 
-                        <input type="text" name="" id="" className=' block w-72 py-2.3 px-0 text-sm bg-transparent border-0 border-b-2 border-[#3C3633] appearance-none dark:focus:border-[#E0CCBE] focus:outline-non focus:outline-none focus:ring-0 focus-text-white focus:border-[#E0CCBE] peer' />
+                    <form className="w-full max-w-lg">
 
-                        <label htmlFor="" className='absolute text-sm text-[#747264] duration-300 -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer=focus:left-0 peer-focus:text-[#E0CCBE] peer-focus:dark:text-[#E0CCBE] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>
-                            your id
-                        </label>
-                        <AiOutlineLock className=' absolute top-[1px] right-4' />
+                        <div className="flex flex-wrap -mx-3 mb-6">
+                            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
+                                    First Name
+                                </label>
+                                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="First Name" />
 
-                    </div>
+                            </div>
+                            <div className="w-full md:w-1/2 px-3">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
+                                    Last Name
+                                </label>
+                                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Last Name" />
+                            </div>
+                        </div>
+                        <div className="flex flex-wrap -mx-3 mb-6">
+                            <div className="w-full px-3">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+                                    Email
+                                </label>
+                                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="email" placeholder="Email" />
+                            </div>
+                        </div>
+                        <div className="flex flex-wrap -mx-3 mb-6">
+                            <div className="w-full px-3">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+                                    Password
+                                </label>
+                                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
+                            </div>
+                        </div>
+                        <div className="flex flex-wrap -mx-3 mb-6">
+                            <div className="w-full px-3">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+                                    Confirm Password
+                                </label>
+                                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
+                            </div>
+                        </div>
+                        <div className=' text-center '>
+                            <button className=' align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none'>
+                                Create Manager
+                            </button>
+                        </div>
 
-                    <div className=' relative my-4' >
-                        <input type="email" name="" id="" className=' block w-72 py-2.3 px-0 text-sm bg-transparent border-0 border-b-2 border-[#3C3633] appearance-none dark:focus:border-[#E0CCBE] focus:outline-non focus:outline-none focus:ring-0 focus-text-white focus:border-[#E0CCBE] peer' />
-                        {/* if u ask about what is the peer in the css check this video => https://www.youtube.com/watch?v=zxMZtqTHuVI&ab_channel=CodinginFlow <= */}
-                        <label htmlFor="" className='absolute text-sm text-[#747264] duration-300 -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer=focus:left-0 peer-focus:text-[#E0CCBE] peer-focus:dark:text-[#E0CCBE] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>
-                            your name
-                        </label>
-
-                        <BiUserCircle
-                            className=' absolute top-[1px] right-4'
-                        />
-                    </div>
-
-                    <div className=' relative my-4' >
-                        <input
-                            type='tel'
-                            name=""
-                            id=""
-                            className=' block w-72 py-2.3 px-0 text-sm bg-transparent border-0 border-b-2 border-[#3C3633] appearance-none dark:focus:border-[#E0CCBE] focus:outline-non focus:outline-none focus:ring-0 focus-text-white focus:border-[#E0CCBE] peer'
-                            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                        />
-                        {/* if u ask about what is the peer in the css check this video => https://www.youtube.com/watch?v=zxMZtqTHuVI&ab_channel=CodinginFlow <= */}
-                        <label
-                            htmlFor=""
-                            className='absolute text-sm text-[#747264] duration-300 -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer=focus:left-0 peer-focus:text-[#E0CCBE] peer-focus:dark:text-[#E0CCBE] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>
-                            your number
-                        </label>
-
-                        <BiUserCircle
-                            className=' absolute top-[1px] right-4'
-                        />
-                    </div>
-
-
-
-                    <button className='w-full mb-4 text-[20px] mt-6 rounded-full text-[#3C3633] bg-[#747264] hover:bg-[#3C3633] hover:text-[#E0CCBE] transition-colors duration-380 italic' type="submit" >Save</button>
-
-                </form>
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
